ac1da15d93af065f673115fe8a1e4466fafbc463
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-08-03 22:38:49 +0300

    initial changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const name = '';
+const inputRef = document.querySelector('#search-box');
+inputRef.addEventListener('input', fetchCountries(name));
+
+function fetchCountries(name) {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('Error!'));
+}
+
+export { data };
+import { data } from './fetchCountries.js';
