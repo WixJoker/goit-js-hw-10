6db3fb9c2a192d932cf2fa7807e74c60be1197e3
WixJoker <96374210+WixJoker@users.noreply.github.com>
2022-08-07 14:11:07 +0000

    Deploying to gh-pages from @ WixJoker/goit-js-hw-10@b5c8e4b3dc5f389307354c4b54c1af0b4bd7e4e4 🚀

--- index.1bf3cc13.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box").addEventListener("input",void fetch(`https://restcountries.com/v3.1/name/${""}`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((e=>console.log(e))).catch((e=>console.log("Error!"))));
-//# sourceMappingURL=index.1bf3cc13.js.map
--- index.dfc9b10e.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;document.querySelector("#search-box").addEventListener("input",(n="https://restcountries.com/v3.1/name/".concat(""),void fetch(n).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()})).then((function(n){return console.log(n)})).catch((function(n){return console.log("Error!")}))))}();
-//# sourceMappingURL=index.dfc9b10e.js.map
